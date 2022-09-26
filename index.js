d9d8ea0b0bb4e299c7db33bef2ec20ad0fb92b79
DavidDowell <daviddowell1@gmail.com>
2022-09-26 14:30:01 -0400

    created a list of questions for the README.md

--- index.js
+++ index.js
@@ -11,15 +11,69 @@ const questions = () => {
         {
             type: 'input',
             name: 'name',
-            message: 'What is the title of your project?'
+            message: 'What is your name? (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                   console.log("Please enter your name!");
+                   return false; 
+                }
+            }
         },
         {
             type: 'input',
-            about: 'about',
+            name: 'title',
+            message: 'What is the name of your project?'
+        },
+        {
+            type: 'input',
+            name: 'description',
             message: 'Provide some details about your project:'
         },
         {
-            type: ''
+            type: 'checkbox',
+            name: 'installation',
+            message: "What did you install to create this application? (Check all that apply)",
+            choices: ['Node.js', 'inquirer', 'VScode', 'Bootstrap', 'JQuery']
+        },
+        {
+            type: 'input',
+            name: 'uses',
+            message: 'How does someone use this application?'
+        },
+        {
+            type: 'checkbox',
+            name: 'license',
+            message: 'What license(s) did you use? (Choose one)',
+            choices: ['The MIT License', 'Apache License', 'GNU License', 'N/A'],
+            validate: value => {
+                if (value) {
+                    return true;
+                } else {
+                    return "You must choose a value to continue";
+                }
+            },
+        },
+        {
+            type: 'input',
+            name: 'contribute',
+            message: 'Who contributed to this project, and how can others add to it?'
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Has there been any peer reviewed tests on this application?'
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message:'What is your gitHub username?'
+        },
+        {
+            type: 'input',
+            name: 'contact',
+            message:'Please enter your email address, so that questions about this application can be sent your way.'
         }
     ])};
 
