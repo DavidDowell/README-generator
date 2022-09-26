b1eda2d554e6f05ea36a365e4607b75c2d9e14a3
DavidDowell <daviddowell1@gmail.com>
2022-09-26 13:50:59 -0400

    initial setup with added questions

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const path = require('path');
+const generateMarkdown = require('./utils/generateMarkdown')
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the title of your project?'
+        },
+        {
+            type: 'input',
+            about: 'about',
+            message: 'Provide some details about your project:'
+        },
+        {
+            type: ''
+        }
+    ])};
+
+    questions().then(answers => console.log(answers))
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
